9e130c872e8eeb0cf320e8e2f31c812b54a75368
michellcamargom@gmail.com <michellcamargom@gmail.com>
2022-09-07 17:15:44 -0300

    Initial commit

---
+++ core/__init__.py
@@ -0,0 +1,10 @@
+import datetime
+
+class SystemImfo:
+    def __init__(self):
+        pass
+    @staticmethod
+    def get_time():
+        now = datetime.datetime.now()
+        answer = 'são {} horas e {} minutos.'.format(now.hour, now.minute)
+        return answer
